24c095e72cdd8bbadc0e5c8277ee048db38b3242
Dev Sharma <ginnisharma1972000@gmail.com>
2022-03-29 12:35:28 +0530

    updating ui

--- src/List.jsx
+++ src/List.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 export default function List(props) {
+  const [state, setState] = useState("true");
+  const lineThrough = () => {
+    if (state) setState(false);
+    else setState(true);
+  };
   return (
     <div className="todo_style">
-      <i
+      <DeleteForeverIcon
         className="fa fa-times"
         aria-hidden="true"
         onClick={() => {
           props.onSelect(props.id);
         }}
       />
-      <li> {props.text} </li>
+
+      <li
+        onClick={lineThrough}
+        style={{ textDecoration: state ? "none" : "line-through" }}
+      >
+        {props.text}
+      </li>
     </div>
   );
 }
